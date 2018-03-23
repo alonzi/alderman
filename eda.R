ffeb2df865f79fbc34f146000bc023e9ed31aff0
pete alonzi <lpa2a@virginia.edu>
2018-03-23 13:43:46 -0400

    make explanitory histogram

--- eda.R
+++ eda.R
@@ -38,13 +38,16 @@ ggplot(data=tail(E_tib,58888), aes(`Total Checkouts`)) +
   geom_histogram() +
   scale_y_log10()
 
-# checkouts v days since
-# no outliers
+# checkouts vs days since
+# no outliers checkouts
 ggplot(data=tail(E_tib,58888),aes(x=days_since_last_checkout,y=`Total Checkouts`)) +
   geom_hex()
 #with outliers
 ggplot(data=E_tib,aes(x=days_since_last_checkout,y=`Total Checkouts`)) +
   geom_hex()
+
+### conserved quantity maximum - days + checkouts = C
+### actually i don't like this
 
 
 #####################
@@ -57,7 +60,42 @@ ggplot(data=E_tib, aes(days_since_last_checkout)) +
 # annotated and zoomed
 ggplot(data=E_tib, aes(days_since_last_checkout)) + 
   geom_histogram(bins=2000/7) +
-  geom_vline(size=2,xintercept=365, color="red") +
-  geom_vline(size=2,xintercept=2*365, color="blue") +
-  geom_vline(size=2,xintercept=5*365, color="green") +
+  geom_vline(size=1.2,xintercept=365, color="red") +
+  geom_vline(size=1.2,xintercept=2*365, color="blue") +
+  geom_vline(size=1.2,xintercept=3*365, color="green") +
+  geom_vline(size=1.2,xintercept=4*365, color="cyan") +
+  geom_vline(size=1.2,xintercept=5*365, color="magenta") +
+  scale_x_continuous(limits = c(0,5.5*365))
+
+### Apply a fourier transform to pick out all the details
+
+# spike at 3000
+ggplot(data=E_tib, aes(days_since_last_checkout)) + 
+  geom_histogram()  +
+  scale_x_continuous(limits = c(2000,4000))
+ggplot(data=E_tib, aes(days_since_last_checkout)) + 
+  geom_histogram(bins = 2000/7)  +
+  scale_x_continuous(limits = c(2000,4000))
+ggplot(data=E_tib, aes(days_since_last_checkout)) + 
+  geom_histogram(bins = 50/7)  +
+  scale_x_continuous(limits = c(3000,3050))
+
+### ?? what happened 3030 days since data was collected ??
+
+# total checkouts
+ggplot(data=tail(E_tib,58888), aes(`Total Checkouts`)) + 
+  geom_histogram() +
+  scale_y_log10()
+ggplot(data=tail(E_tib,58888), aes(`Total Checkouts`)) + 
+  geom_histogram(bins=300)
+ggplot(data=tail(E_tib,58888), aes(`Total Checkouts`)) + 
+  geom_histogram(bins=300) +
+  scale_y_log10()
+
+### looks exponential to me
+
+# checkouts vs days since
+# no outliers
+ggplot(data=tail(E_tib,58888),aes(x=days_since_last_checkout,y=`Total Checkouts`)) +
+  geom_hex(bins=2000/14)+
   scale_x_continuous(limits = c(0,2000))
