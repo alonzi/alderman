3441864e65197c41f3fe3fa713454b49ffcc6c19
pete alonzi <lpa2a@virginia.edu>
2018-03-23 21:06:51 -0400

    update files from meeting

--- eda.R
+++ eda.R
@@ -3,7 +3,7 @@
 # contents: exploratory data analysis
 # date: 3/23/18
 # github: https://github.com/alonzi/alderman
-# purpose: initial exploration of Morton data set
+# purpose: initial exploration of data set
 
 ######################
 # set up environment
