5940fc09d6aaad00f3a9c7526e1e275eec80e5fb
pete alonzi <lpa2a@virginia.edu>
2018-07-19 12:45:02 -0400

    write script to generate liaison lists

---
+++ phase2-picklistmodels/src/generate_liaison_lists.R
@@ -0,0 +1,22 @@
+# liaison list generator script
+# lpa2a
+# 2018-07-19
+# usage: bash$ Rscript src/generate_liaison_lists.R
+
+# Load Necessary Functions
+source("./src/data_cleaner.R")
+
+# Configure To run on desired data
+datapath <- "./dat"
+input_files <- list.files(datapath)
+input_files <- paste(datapath,input_files,sep="/")
+output_files <- paste(paste("./liaison_review",list.files(datapath),sep="/"),"review.csv",sep=".")
+
+# Turn the crank
+tibbles <- lapply(input_files,data_cleaner)
+
+# make output files
+for (i in 1:length(output_files)){
+  print(paste("writing file",output_files[i]))
+  write.csv(tibbles[i],file = output_files[i])
+}
